8ee0b035491818353194a2e12de6cabc370d8fce
Erin Currie <ErinLCurrie@gmail.com>
2015-08-17 16:53:38 -0400

    Clear and focus the name field after adding a student.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,14 +1,30 @@
-var Megatask = {
-  author: "Erin",
-  hisDeal: "Who the heck knows?",
-  newStudentForm: $('#new_student_form'),
-  submitHandler: function(ev) {
-    alert('What?');
-  },
-  start: function() {
-    this.newStudentForm.submit(this.submitHandler);
+var Megaroster = function() {
 
-  }
+
+  this.init = function() {
+    var self = this;
+    this.students = [];
+
+    $('#new_student_form').on('submit', function(ev) {
+      ev.preventDefault();
+      var student_name = $(this.student_name).val();
+
+      //Push the student name onto the student array
+      self.students.push(student_name);
+
+      //Add the student to a new list item in the <ol>
+      //$("#header ul").append('<li><a href="/user/messages"><span class="tab">Message Center</span></a></li>');
+
+      $('#students').append('<li class = "list-group-item">' + student_name + '</li>');
+      $(this.student_name)
+        .val('')
+        .focus();
+
+
+    });
+  };
+
 };
 
-Megatask.start();
+var roster = new Megaroster();
+roster.init();
