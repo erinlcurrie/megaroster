c9d1926e6f86ea3661c57f60b506949a69769a03
Erin Currie <ErinLCurrie@gmail.com>
2015-08-18 17:03:40 -0400

    Delete students.

--- js/megaroster.js
+++ js/megaroster.js
@@ -3,7 +3,7 @@ var Megaroster = function() {
 
   this.save = function() {
     try {
-      return (localStorage.students = JSON.stringify(self.students)   );
+      return (localStorage.students = JSON.stringify(self.students));
     }
     catch(err) {
       return false;
@@ -12,9 +12,12 @@ var Megaroster = function() {
 
   this.load = function() {
     try {
-      self.students = JSON.parse(localStorage.students);
-      $.each(self.students, function(index, student_name) {
-        self.appendToList(student_name);
+      var student_data_objects = JSON.parse(localStorage.students);
+      $.each(self.students, function(index, student_data) {
+        var student = new Student();
+        student.init(student_data);
+        student.appendToList();
+        self.students.push(student);
       });
     }
     catch(err) {
@@ -22,32 +25,54 @@ var Megaroster = function() {
     }
   };
 
-  this.appendToList = function(student_name) {
-    // Grab the *template* list item from the page.
-    var li = $('#list_item_template').clone();
-    li.removeAttr('id')
-      .addClass('student')
-      .prepend(student_name)
-      .removeClass('hidden');
-
-    $('#students').append(li);
-  };
+  // this.appendToList = function(student_name) {
+  //   var li = $('#list_item_template').clone();
+  //   li.removeAttr('id')
+  //     .addClass('student')
+  //     .prepend(student_name)
+  //     .removeClass('hidden');
+  //
+  //   $('#students').append(li);
+  // };
 
   this.addStudent = function(student_name) {
-    self.students.push(student_name);
-    self.appendToList(student_name);
+    var student = new Student();
+    student.init({
+      name: student_name
+    });
+
+    self.students.push(student);
+    student.appendToList();
+
+    self.save();
   };
 
   this.init = function() {
     self.students = [];
+    Student.counter = 0;
     self.load();
 
     $(document).on('click', 'button.delete', function(ev) {
-      //Remove it from the array
-      // WAIT UNTIL TMR
-      //Remove it from the <ol>
-      $(this).closest('li').remove();
-      //Update local storage
+      var li = $(this).closest('li');
+
+      // Remove it from the array
+      var id = li.attr('data-id');
+
+      // This is equivalent to the one below.
+      $.each(self.students, function(index, current_student){
+        if (current_student.id.toString() === id.toString()) {
+          self.students.splice(index, 1);
+          return false;
+        }
+      });
+
+      // self.students.splice.indexOf(current_student, 1);
+
+      li.remove();
+      self.save();
+
+      // Update localStorage
+      // WAIT UNTIL TOMORROW
     });
 
     $('#new_student_form').on('submit', function (ev) {
@@ -61,8 +86,6 @@ var Megaroster = function() {
         .focus();
     });
   };
-
-
 };
 
 var roster = new Megaroster();
---
+++ js/student.js
@@ -0,0 +1,45 @@
+var Student = function() {
+  var self = this;
+
+  self.getOrSetId = function(id) {
+    if (!id) {
+      id = Student.counter + 1;
+    }
+    self.incrementCounter(id);
+    return id;
+  };
+
+  self.incrementCounter = function(id) {
+    if (id > Student.counter) {
+      Student.counter = id;
+    }
+
+  };
+
+  self.init = function (properties) {
+    self.name = properties.name;
+    self.id = self.getOrSetId(properties.id);
+  };
+
+  self.appendToList = function() {
+    var li = $('#list_item_template').clone();
+    li.removeAttr('id')
+      .attr('data-id', self.id)
+      .addClass('student')
+      .prepend(self.name)
+      .removeClass('hidden');
+
+    $('#students').append(li);
+  };
+};
+
+Student.getStudentById = function(id) {
+  var student;
+  $.each(roster.students, function(index, current_student) {
+    if (current_student.id.toString() === id.toString) {
+      student = current_student;
+      return false;
+    }
+  });
+  return student;
+};
